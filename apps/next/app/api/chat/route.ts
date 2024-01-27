df308903b675685c715872d0e4df8452b999b129
Xiangyi Li <venusli370306@gmail.com>
2024-01-27 01:31:16 -0800

    fix: chat max token and delete story book (#63)

--- apps/next/app/api/chat/route.ts
+++ apps/next/app/api/chat/route.ts
@@ -1,4 +1,3 @@
-// @ts-nocheck
 import { NextRequest } from 'next/server'
 import { OpenAI } from 'openai'
 import { ChatCompletionChunk, ChatCompletionMessage } from 'openai/resources/chat/completions'
@@ -16,17 +15,23 @@ function messageReducer(
   const reduce = (inputAcc: ChatCompletionMessage, delta: ChatCompletionChunk) => {
     const acc = { ...inputAcc }
     for (const [key, value] of Object.entries(delta)) {
+      // @ts-ignore
       if (acc[key] === undefined || acc[key] === null) {
+        // @ts-ignore
         acc[key] = value
+        // @ts-ignore
       } else if (typeof acc[key] === 'string' && typeof value === 'string') {
+        // @ts-ignore
         ;(acc[key] as string) += value
+        // @ts-ignore
       } else if (typeof acc[key] === 'object' && !Array.isArray(acc[key])) {
+        // @ts-ignore
         acc[key] = reduce(acc[key], value)
       }
     }
     return acc
   }
-
+  // @ts-ignore
   return reduce(previous, item.choices[0].delta) as ChatCompletionMessage
 }
 
@@ -40,25 +45,18 @@ export async function POST(req: NextRequest) {
       const { model, messages } = await req.json()
 
       while (true) {
-        // console.log('message:', messages)
         const stream = await openai.chat.completions.create({
           messages,
-          //functions: null,
           model: models[model as modelOptions],
           stream: true,
+          max_tokens: 40,
         })
         let message = {} as ChatCompletionMessage
         for await (const part of stream) {
           message = messageReducer(message, part)
-          // console.log(part)
-          //res.write(JSON.stringify(part));
-          //s.push(JSON.stringify(part));
           controller.enqueue(`${JSON.stringify(part.choices[0].delta)}\0`)
-          // console.log(part)
         }
         messages.push(message)
-        //console.log(message);
-        // If there is no function call, we're done and can exit this loop
         if (!message.function_call) {
           break
         }
--- apps/storybook/types.d.ts
+++
@@ -1,7 +0,0 @@
-import { config } from '@my/ui'
-
-export type Conf = typeof config
-
-declare module '@my/ui' {
-  interface TamaguiCustomConfig extends Conf {}
-}
